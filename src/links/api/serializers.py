f2e76088da6a1663dd3072bbb86b17b70d52d3dd
Anton Danilyuk <danilyukanton1@gmail.com>
2026-05-11 14:58:53 +0300

    refactor CreateLinkView and serializer to add full short_url to response

--- src/links/api/serializers.py
+++ src/links/api/serializers.py
@@ -1,11 +1,19 @@
 from rest_framework import serializers
-
+from django.urls import reverse
 from links.models import Link
 
 
-class LinkSerializer(serializers.ModelSerializer):
+class LinkSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    original_url = serializers.URLField()
+    short_code = serializers.CharField(read_only=True)
+    short_url = serializers.SerializerMethodField()
+    clicks = serializers.IntegerField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
 
-    class Meta:
-        model = Link
-        fields = ('id', 'original_url', 'short_code', 'clicks', 'created_at')
-        read_only_fields = ('short_code', 'clicks', 'created_at',)
+    def get_short_url(self, obj):
+        request = self.context.get('request')
+        if request and obj.short_code:
+            short_url = reverse('redirect', kwargs={'short_code': obj.short_code})
+            return request.build_absolute_uri(short_url)
+        return None
--- src/links/api/views.py
+++ src/links/api/views.py
@@ -1,17 +1,34 @@
 from rest_framework import generics
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.shortcuts import get_object_or_404, redirect
 from links.models import Link
 from links.api.serializers import LinkSerializer
 from links.services import generate_short_code
+from rest_framework import status
 
-class CreateLinkView(generics.CreateAPIView):
-    queryset = Link.objects.all()
-    serializer_class = LinkSerializer
 
-    def perform_create(self, serializer):
+class CreateLinkView(APIView):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        original_url = request.data.get('original_url')
+
+        if not original_url:
+            return Response(
+                {'error': 'original_url is required'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        existing_link = Link.objects.filter(original_url=original_url).first()
+        if existing_link:
+            serializer = LinkSerializer(existing_link, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
         short_code = generate_short_code()
-        serializer.save(short_code=short_code)
+        link = Link.objects.create(original_url=original_url, short_code=short_code)
+        serializer = LinkSerializer(link, context={'request': request})
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 class LinkRedirectView(APIView):
 
