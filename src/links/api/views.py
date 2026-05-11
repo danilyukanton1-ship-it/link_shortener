7fa7ab9591d548fa5601ea49c25666a2ad86ef9a
Anton Danilyuk <danilyukanton1@gmail.com>
2026-05-11 13:30:41 +0300

    create services for generation a short code, and create api views

---
+++ src/links/api/views.py
@@ -0,0 +1,28 @@
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404, redirect
+from links.models import Link
+from links.api.serializers import LinkSerializer
+from links.services import generate_short_code
+
+class CreateLinkView(generics.CreateAPIView):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+
+    def perform_create(self, serializer):
+        short_code = generate_short_code()
+        serializer.save(short_code=short_code)
+
+class LinkRedirectView(APIView):
+
+    def get(self, request, short_code):
+        link = get_object_or_404(Link, short_code=short_code, is_active=True)
+        link.clicks += 1
+        link.save()
+        return redirect(link.original_url)
+
+class ListStatsView(generics.RetrieveAPIView):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
+    lookup_field = 'short_code'
